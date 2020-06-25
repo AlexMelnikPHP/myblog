45643476d74874b5c66dcdfb339430c8f53c5944
AlexMel <alexey.melnik2000@gmail.com>
2020-06-25 20:19:52 +0300

    first commit

--- database/migrations/2020_06_25_165831_create_articles_table.php
+++ database/migrations/2020_06_25_165831_create_articles_table.php
@@ -15,6 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 400 );
+            $table->text('content')->nullable();
+            $table->boolean('is_draft')->default(true);
             $table->timestamps();
         });
     }
