bcd932ad23b794255ddc0040d719da5fab2263c1
AlexMel <alexey.melnik2000@gmail.com>
2020-06-27 22:16:33 +0300

    first commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        Post::class => PostPolicy::class,
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
